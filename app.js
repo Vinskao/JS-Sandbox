0aa45fc559dabe329d7f483d8a4063f97d775fd8
Vinskao <tianyikao@gmail.com>
2023-06-02 00:48:58 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//log to console
+console.log("Hello World!");
+console.log(123);
+console.log(true);
+var greetings = "Hello World!";
+console.log(greetings);
+console.log([1,2,3,4]);
+console.log({a:1,b:2,c:3});
+console.table({a:1,b:2});
+
+console.error('error');
+console.clear();
+
+console.warn('warning');
+console.time('hello');
+  console.log('hello');
+  console.log('hello');
+  console.log('hello');
+  console.log('hello');
+  console.log('hello');
+console.timeEnd('hello');
+
+
+/*
+  multi
+  line
+  comment
+*/
