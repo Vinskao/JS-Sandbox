73ffa33f7f21d5b04c852e848622b9c7d2d3f829
Vinskao <tianyikao@gmail.com>
2023-06-04 23:47:01 +0800

    Initial commit

--- app.js
+++ app.js
@@ -1,45 +1,101 @@
-//log to console
-console.log("Hello World!");
-console.log(123);
-console.log(true);
-var greetings = "Hello World!";
-console.log(greetings);
-console.log([1,2,3,4]);
-console.log({a:1,b:2,c:3});
-console.table({a:1,b:2});
+// // //log to console
+// // console.log("Hello World!");
+// // console.log(123);
+// // console.log(true);
+// // var greetings = "Hello World!";
+// // console.log(greetings);
+// // console.log([1,2,3,4]);
+// // console.log({a:1,b:2,c:3});
+// // console.table({a:1,b:2});
 
-console.error('error');
-console.clear();
+// // console.error('error');
+// // console.clear();
 
-console.warn('warning');
-console.time('hello');
-  console.log('hello');
-  console.log('hello');
-  console.log('hello');
-  console.log('hello');
-  console.log('hello');
-console.timeEnd('hello');
+// // console.warn('warning');
+// // console.time('hello');
+// //   console.log('hello');
+// //   console.log('hello');
+// //   console.log('hello');
+// //   console.log('hello');
+// //   console.log('hello');
+// // console.timeEnd('hello');
 
-//var,let,const
-var name = 'John';
-console.log(name);
-name = 'Jane';
+// // //var,let,const
+// // var name = 'John';
+// // console.log(name);
+// // name = 'Jane';
 
-console.log(name);
-//init var
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting); 
-//letters, numbers, _, $
-//can not start with number
-// multi word vars
-var firstname = 'John';// camel case
-var firstname = 'Jane';// underscore
-var firstname = 'Tom';// pascal case
+// // console.log(name);
+// // //init var
+// // var greeting;
+// // console.log(greeting);
+// // greeting = 'Hello';
+// // console.log(greeting); 
+// // //letters, numbers, _, $
+// // //can not start with number
+// // // multi word vars
+// // var firstname = 'John';// camel case
+// // var firstname = 'Jane';// underscore
+// // var firstname = 'Tom';// pascal case
 
-/*
-  multi
-  line
-  comment
-*/
+
+// //let
+// //let name = 'John'
+// //console.log(name);
+// //name = 'Jane';
+// //console.log(name);
+
+// //const
+// // const name = 'John';
+// // console.log(name);
+
+// // const person = {
+// //   name: 'John',
+// //     age: 20 
+// // }
+
+// // person.name = 'Jane';
+// // person.age = 30;
+
+// // // console.log(person);
+
+// // const numbers = [1,2,3,4,5,6,7,8,9,]
+// // numbers.push(10);
+
+// // console.log(numbers);
+
+// // primitive
+// // //string
+// // const name = 'John';
+// // //number
+// // const age = 20;
+// // //boolean
+// // const haskids=true;
+// //null
+// // const car = null;
+// // //undefined
+// // let test;
+// // //symbol
+// // const sym = symbol();
+// // console.log(typeof haskids);
+
+// //refence types - objects
+// //array
+// const hobbies = [ 'coding', 'music' ]
+// //object literal
+// const address = {
+//   city: 'New York',
+//   state: 'NY'
+// }
+let val
+
+//number to string
+
+//toString()
+val = (5).toString();
+val = (true).toString();
+val = String(new Date());
+//output
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
