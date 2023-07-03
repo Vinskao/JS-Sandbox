6224d24961ce24ceea968672b09aa9c0e1f49b8f
Tianyi Kao (Max) <59313635+Vinskao@users.noreply.github.com>
2023-07-03 03:51:35 +0000

    Initial commit

--- app.js
+++ app.js
@@ -46,3 +46,25 @@ if(color === 'yellow'){
 } else if (color === 'blue'){
     console.log("color is blue");
 } 
+//logical operators
+const name = 'kao';
+const age = 17;
+//and &&
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if (age >= 13 && age < 19){
+    console.log(`${name} is a teen`);
+} else {
+    console.log(`${name} is an adult`);
+}
+
+//or|| just one is true
+if (age < 16 || age> 65){
+    console.log(`${name} can not run in race`);
+}else {
+    console.log(`${name} can run in a race`);
+}
+
+
+//ternary operator 
+console.log(id === 100 ? 'correct' : 'wrong');
