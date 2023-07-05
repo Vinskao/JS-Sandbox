cc5a338ac540ba7cacd7e561a3a1adb9f1bccf6f
Tianyi Kao (Max) <59313635+Vinskao@users.noreply.github.com>
2023-07-05 09:38:33 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const color = 'yellow';
-switch(color) {
-    case 'red':
-        console.log('color is red');
-        break; 
-    case 'green':
-        console.log('color is green');
-        break; 
-    default:
-        console.log('color is not red or green');
-        break; 
+//function declaration
+function greet(firstname = 'm',lastname = 'kao'){
+    // if(typeof firstname === 'undefined'){firstname= 'mk'}
+    // console.log('Hello');
+    return 'hello '+ firstname + ' ' + lastname;
 }
 
-switch(new Date().getDay()) {
-    case 0:
-        day = 'Sunday';
-        break;
-    case 1:
-        day = 'Monday';
-        break;
-    case 2:
-        day = 'Tuesday';
-        break;
-    case 3:
-        day = 'Wednesday';
-        break;
-    case 4:
-        day = 'Thursday';
-        break;
-    case 5:
-        day = 'Friday';
-        break;
-    case 6:
-        day = 'Saturday';
-        break;
+// console.log(greet('mkao'));
+
+const square = function (x = 3 ){
+    return x*x;
+};
+
+// console.log(square());
+
+//immediately invoable function expressions - IIFES
+// (function(){
+//     console.log('IIFE Ran..');
+// })();
+
+// (function(name){
+//     console.log('hello '+  name );
+// })('brad');
+
+//property methods
+
+const todo = {
+    add: function (){
+        console.log('add todo..');
+    },
+    edit: function(id){
+        console.log(`edit todo ${id}`);
+    }
 }
 
-console.log(`today is ${day}`);
+todo.delete = function(){
+    console.log('delete todo..');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
